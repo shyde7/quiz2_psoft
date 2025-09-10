92e831655f56f99773e83683047800ded2294d60
Anbcodes <me@anb.codes>
2025-09-10 11:13:01 -0400

    Added another log

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-	x=5;
+	      x=5;
+        System.out.println("Hi from Anbcodes!");
     }
 }
